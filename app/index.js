930e1ddd73c5d604dc4d4b2aea629bee9c8c5399
Amit Jha <amitjha.8080@gmail.com>
2025-04-23 13:08:44 +0530

    Initial commit: CI/CD microservice with Docker & GitHub Actions

---
+++ app/index.js
@@ -0,0 +1,13 @@
+// app/index.js
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Microservice 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
